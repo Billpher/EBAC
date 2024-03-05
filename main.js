b4613e01c8736a2fcf0ed3db844fb7e706f34cca
Lucas <vieiralucas1199@gmail.com>
2024-03-05 20:34:37 -0300

    ajuste do jquery

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
-    document.querySelector('header button');
-    $('header button');
+    //selecionar pelo javascript
+    //document.querySelector('header button');
+
+    //selecionar pelo jquery
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const enderecoDaImagem = $('#imagemNova').val();
+
+        const novoItem = $('<li style="display: none"></li>');
+
+        $(`
+            <img src='${enderecoDaImagem}' />
+        `).appendTo(novoItem);
+
+        $(`
+            <div class='overlay-img-link'>
+                <a href'${enderecoDaImagem}' target='_blank' title'Imagem em tamanho real'> Ver imagem em tamanho real </a>
+            </div>
+        `).appendTo(novoItem);
+
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(3000);
+
+        $('#imagemNova').val(' ');
+
+    })
 })

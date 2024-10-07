57f83419b4692d53cd96e2fd49ccdcccb4c7d0e3
Lucas <vieiralucas1199@gmail.com>
2024-10-07 12:06:42 -0300

    atividade

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-const carro = {
-    modelo: 'uno',
-    ano: 2019,
-    fabricacao: 2020,
-    acelerar: function() {
-        console.log('vai')
-    }
-}
-
-function Carro(modelo, ano, fabricacao) {
-    this.modelo = modelo;
+function Aluno(aluno, idade, ano) {
+    this.aluno = aluno;
+    this.idade = idade;
     this.ano = ano;
-    Carro.fabricacao = fabricacao;
-    Carro.acelerar = function() {
-        console.log('vai');
-    }
 }
 
-const carroAna = new Carro('uno', 2019, 2020);
-const carrojoao = new Carro('fiesta', 2020, 2024);
+function Professora(professora, sala, aluno) {
+    this.professora = professora;
+    this.sala = sala;
+    Aluno.call(this, aluno);
+}
 
-console.log(carroAna.modelo);
-console.log(carroAna['modelo']);
+function Escola(escola, professora, sala, aluno, idade, ano) {
+    this.escola = escola;
+    Professora.call(this, professora, sala);
+    Aluno.call(this, aluno, idade, ano)
+}
+
+const escola1 = new Escola('Unifev','maria',10,'Pedro',22,'5 ano');
+const escola2 = new Escola('Etec','Antonio',3,'Clara',17,'3 colegial');
+const escola3 = new Escola('Unifev','Carlos',11,'Diego',20,'2 ano');
+
+console.log(escola1);
+console.log(escola2);
+console.log(escola3);

c7eb4cf574d5d6dad4ee073aa8da8592a57fb4df
Lucas <vieiralucas1199@gmail.com>
2024-10-31 11:57:57 -0300

    ajsute do exercicio

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function conta(a, b) {
+    console.log(a * b);
+}
+function saudacao(nome) {
+    console.log(`Olá ${nome}`);
+}
+conta(10, 20);
+saudacao('Roberto');
--- src/exercicio.ts
+++ src/exercicio.ts
@@ -1,5 +1,11 @@
-function conta(a: number, b:number): number {
-
-    return a * b;
+function conta(a: number, b:number) {
+    console.log(a * b);
     
 }
+
+function saudacao(nome: string) {
+    console.log(`Olá ${nome}`)
+}
+
+conta(10,20);
+saudacao('Roberto');

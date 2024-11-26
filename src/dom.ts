f7e3259f06b37826033dbdf1480bb125bc838f75
Lucas <vieiralucas1199@gmail.com>
2024-11-26 11:05:12 -0300

    ajuste do projeto

--- src/dom.ts
+++
@@ -1,7 +0,0 @@
-const campoNome = document.getElementById('nome');
-const formulario = document.getElementsByTagName('form');
-const botao = document.getElementById('btnEnviar');
-
-botao?.addEventListener('click', (e: MouseEvent) => {
-    e.pageX
-})
